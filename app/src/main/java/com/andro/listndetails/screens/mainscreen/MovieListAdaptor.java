0d5789b8a5e397477c5c40c3aaad01441b920d18
Rana Ahsan Ali <ranaahsanali@gmail.com>
2017-01-14 18:46:59 +0400

    Consume api and populate

--- app/src/main/java/com/andro/listndetails/screens/mainscreen/MovieListAdaptor.java
+++ app/src/main/java/com/andro/listndetails/screens/mainscreen/MovieListAdaptor.java
@@ -1,8 +1,72 @@
 package com.andro.listndetails.screens.mainscreen;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andro.listndetails.R;
+import com.andro.listndetails.models.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by andro on 1/14/2017.
  */
 
-public class MovieListAdaptor {
+public class MovieListAdaptor extends RecyclerView.Adapter<MovieListItem>{
+
+    private List<Result> mResults = new ArrayList<Result>();
+
+    private MovieListAdapterInterface mMovieListAdapterInterface;
+
+    @Override
+    public int getItemCount() {
+        return mResults.size();
+    }
+
+    @Override
+    public MovieListItem onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.list_item_view, parent, false);
+
+        return new MovieListItem(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(MovieListItem holder, int position) {
+        holder.populate(mResults.get(position));
+        holder.setMovieItemInterface(mMovieItemInterface);
+
+    }
+
+    private MovieListItem.MovieItemInterface mMovieItemInterface = new MovieListItem.MovieItemInterface() {
+        @Override
+        public void onItemClicked(Result result) {
+            if (mMovieListAdapterInterface != null) {
+                mMovieListAdapterInterface.onItemClicked(result);
+            }
+        }
+    };
+
+    public void setData(List<Result> results) {
+        mResults = results;
+    }
+
+    public void addData(List<Result> results) {
+        mResults.addAll(results);
+        notifyDataSetChanged();
+    }
+
+    public void setMovieListAdapterInterface(MovieListAdapterInterface movieListAdapterInterface) {
+        this.mMovieListAdapterInterface = movieListAdapterInterface;
+    }
+
+    public interface MovieListAdapterInterface {
+        void onItemClicked(Result result);
+    }
+
 }
--- app/src/main/java/com/andro/listndetails/screens/mainscreen/MovieListItem.java
+++ app/src/main/java/com/andro/listndetails/screens/mainscreen/MovieListItem.java
@@ -1,8 +1,64 @@
 package com.andro.listndetails.screens.mainscreen;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.andro.listndetails.R;
+import com.andro.listndetails.models.Result;
+import com.andro.listndetails.utils.ImageUtils;
+
 /**
  * Created by andro on 1/14/2017.
  */
 
-public class MovieListItem {
+public class MovieListItem extends RecyclerView.ViewHolder{
+
+    public static final String POSTER_SIZE_W_185 = "w185";
+
+    private TextView mTitleTextView;
+    private TextView mDateTextView;
+    private ImageView mImageView;
+
+    private Result mResult;
+
+    private MovieItemInterface mMovieItemInterface;
+
+    public MovieListItem(View itemView) {
+        super(itemView);
+
+        mTitleTextView = (TextView) itemView.findViewById(R.id.listItemViewTitleTextView);
+        mDateTextView = (TextView) itemView.findViewById(R.id.listItemViewDateTextView);
+        mImageView = (ImageView) itemView.findViewById(R.id.listItemViewImageView);
+
+    }
+
+    public void populate(Result result) {
+
+        mResult = result;
+
+        mTitleTextView.setText(result.getTitle());
+        mDateTextView.setText(result.getReleaseDate());
+        ImageUtils.getImage(result.getPosterPath(), POSTER_SIZE_W_185, mImageView, -1);
+
+        itemView.setOnClickListener(itemClickListener);
+    }
+
+    private View.OnClickListener itemClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (mMovieItemInterface != null) {
+                mMovieItemInterface.onItemClicked(mResult);
+            }
+        }
+    };
+
+    public void setMovieItemInterface(MovieItemInterface movieItemInterface) {
+        this.mMovieItemInterface = movieItemInterface;
+    }
+
+    public interface MovieItemInterface {
+        void onItemClicked(Result result);
+    }
 }
