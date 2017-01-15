f74fcbfab5f995a6c88a1f0030233ced942187cd
Rana Ahsan Ali <ranaahsanali@gmail.com>
2017-01-15 14:32:14 +0400

    Add deeplinks

--- app/src/main/java/com/andro/listndetails/MainActivity.java
+++ app/src/main/java/com/andro/listndetails/MainActivity.java
@@ -1,11 +1,19 @@
 package com.andro.listndetails;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.andro.listndetails.applicationbase.BaseActivity;
 import com.andro.listndetails.screens.mainscreen.MainScreenFragment;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 public class MainActivity extends BaseActivity {
+
+    public static final String DEEPLINK_MESSAGE_KEY = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,20 @@ public class MainActivity extends BaseActivity {
 
         setcontentView();
         addFragment(new MainScreenFragment(), savedInstanceState);
+
+        String action = getIntent().getAction();
+
+        if (action.equalsIgnoreCase(Intent.ACTION_VIEW)) {
+            Uri data = getIntent().getData();
+
+            String parameter = data.getQueryParameter(DEEPLINK_MESSAGE_KEY);
+
+            try {
+                Toast.makeText(getApplicationContext(),
+                        URLDecoder.decode(parameter, "utf-8"), Toast.LENGTH_LONG).show();
+            } catch (UnsupportedEncodingException e) {
+                Toast.makeText(getApplicationContext(), parameter, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
